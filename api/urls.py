c8684ae3773c55b1392013ad96aaa3dd50590565
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-05 22:39:56 -0600

    Add endpoint for returning a single user_activity object

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('exercises', views.exercise_index, name="exercise_index"),
-    path('exercises/<int:exercise_id>', views.exercise_show)
+    path('exercises/<int:exercise_id>', views.exercise_show),
+    path('user_activities/<int:user_activity_id>', views.user_activity),
 ]
