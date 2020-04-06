9293b1ef783a9f99a0d067f4d1ce220a56607e63
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-06 12:41:41 -0600

    Update get_forecast function to exclude all pieces of response except for the 'currently' block from Darksky

--- api/external_services/darksky_service.py
+++ api/external_services/darksky_service.py
@@ -4,9 +4,9 @@ from django.conf import settings
 class DarkskyService:
 
     def get_json(self, uri):
-        response = requests.get(f' https://api.darksky.net/forecast/{settings.DARKSKY_API_KEY}/{uri}')
+        response = requests.get(f'https://api.darksky.net/forecast/{settings.DARKSKY_API_KEY}/{uri}')
         return response.json()
 
     def get_forecast(self, lat, lng, time):
-        uri = f'{lat},{lng},{time}'
+        uri = f'{lat},{lng},{time}?exclude=hourly,daily,alerts,flags'
         return self.get_json(uri)
