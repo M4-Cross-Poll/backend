458ad7ea7b0da0b2554a1d358f360db9a9206897
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-05 21:00:22 -0600

    Add exercise_show action to views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import JsonResponse
+from api.models import *
+from api.serializers import ExerciseSerializer
 # Create your views here.
 
 def exercise_index(request):
     return HttpResponse("You found me")
+
+def exercise_show(request, exercise_id):
+    queryset = Exercise.objects.get(id=exercise_id)
+    serializer = ExerciseSerializer(queryset)
+    return JsonResponse(serializer.data)
+
+def user_activity(request, user_activity_id):
+    
