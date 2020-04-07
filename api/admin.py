8d5f820d03443339484e8acba4ff03cefa4ff3a2
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-07 14:48:53 -0600

    Add admin view for ExerciseMuscleGroups table

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Activity, MuscleGroup, Exercise, User, ScheduledActivity
+from .models import *
 # Register your models here.
 
 class ActivityAdmin(admin.ModelAdmin):
@@ -11,7 +11,7 @@ class MuscleGroupAdmin(admin.ModelAdmin):
 
 
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'img_url', 'equipment', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'description', 'img_url', 'equipment', 'instructions', 'created_at', 'updated_at')
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -22,8 +22,12 @@ class ScheduledActivityAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'location', 'forecast', 'forecast_img', 'activity', 'user', 'created_at', 'updated_at')
 
 
+class ActivityMuscleGroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'activity_id', 'muscle_group_id')
+
 admin.site.register(Activity, ActivityAdmin)
 admin.site.register(MuscleGroup, MuscleGroupAdmin)
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(ScheduledActivity, ScheduledActivityAdmin)
+admin.site.register(ActivityMuscleGroups, ActivityMuscleGroupAdmin)
