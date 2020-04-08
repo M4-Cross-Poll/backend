1a26f9fc68b96924722bb002f92a0d8c7611fb8b
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-08 10:59:49 -0600

    Update route for scheduled activity show

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('exercises', views.exercise_index, name="exercise_index"),
     path('exercises/<int:exercise_id>', views.exercise_show),
-    path('scheduled_activity/<int:scheduled_activity_id>', views.scheduled_activity_show),
+    path('scheduled_activities/<int:scheduled_activity_id>', views.scheduled_activity_show),
 ]
