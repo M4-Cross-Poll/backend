5698a542a1de715692eac96ecc6a3dd58b926ca0
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-15 17:31:56 -0600

    Change response from GeocodeService to be less truncated so that view has access to the formatted address returned by Google

--- api/external_services/geocode_service.py
+++ api/external_services/geocode_service.py
@@ -13,6 +13,6 @@ class GeocodeService:
         api_key = os.getenv('GEOCODE_API_KEY')
         coordinates_json = self.get_json(f'address={location}&key={api_key}')
         if coordinates_json["status"] == "OK":
-            return coordinates_json["results"][0]["geometry"]["location"]
+            return coordinates_json["results"][0]
         else:
             raise NameError("Invalid Location")
