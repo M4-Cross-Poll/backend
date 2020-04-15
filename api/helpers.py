ca1333d726695761edd6aeaf7c53f7fcdc1f9fc4
Dylan Connolly <dylanconnolly145@gmail.com>
2020-04-15 17:30:53 -0600

    Create helper function to parse a location returned from Geocode service and just grab it's city and state

--- api/helpers.py
+++ api/helpers.py
@@ -2,3 +2,6 @@ from datetime import datetime
 
 def parse_date(date_string):
     return f"{date_string}T12:00:00"
+
+def parse_location(full_location):
+    return ",".join(full_location.split(',')[:2])
